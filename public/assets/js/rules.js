a3ca6f169418b32b49becc544a7e1479d534773a
Gabor Biro <gabor.biro@yahoo.com>
2022-10-16 23:38:41 +0100

    Hidden some unnecessary hours

--- public/assets/js/rules.js
+++ public/assets/js/rules.js
@@ -1,3 +1,4 @@
+const Folded = "Folded cycles";
 const Bakerloo = "Bakerloo";
 const Central = "Central";
 const Circle = "Circle";
@@ -12,25 +13,51 @@ const Overground = "Overground";
 const Piccadilly = "Piccadilly";
 const Victoria = "Victoria";
 const WaterlooCity = "Waterloo & City";
+const BusTram = "Bus and Tram"
+const NationalRail = "National Rail"
+const River = "River"
+const EmiratedAir = "Emirates Air Line"
+const Coaches = "Coaches"
 
+const AllowedFolded = "You can take folded cycles anywhere, at any time on all transport services. However, on buses the driver can decide not to let you travel if it's too busy.";
 const AllowedAnywhere = "Allowed anywhere";
 const NotAllowed = "Not allowed";
+const NotAllowedEver = "Never allowed";
+const NotAllowedBusesTrams = "Cycles cannot be taken on buses and trams";
+const AllowedNationalRail = "Use <a target='_blank' href='http://plusbike.nationalrail.co.uk/'>PlusBike</a> to find out where you can take cycles on National Rail services.";
+const AllowedRiver = "Most <a target='_blank' href='https://tfl.gov.uk/modes/river/'>river services</a> allow non-folded cycles but you should check with them before you travel.";
+const AllowedEmiratesAir = "You can take a non-folded cycle at any time.";
+const AllowedCoaches = "You need to check with the <a target='_blank' href='https://tfl.gov.uk/modes/coaches/coach-companies'>coach company</a> to see if you can take a non-folded cycle.";
+const AllowedBakerloo = "Between Queen's Park and Harrow & Wealdstone";
+const AllowedCentral = "Between Leyton, Epping and Newbury Park (via Hainault only)\nBetween White City, Ealing Broadway and West Ruislip";
+const AllowedJubilee = "Between Stratford and Canning Town\nBetween Finchley Road and Stanmore";
+const AllowedNorthern = "Between Golders Green and Hendon Central\nBetween Colindale and Edgware\nBetween East Finchley, Mill Hill East and High Barnet";
+const AllowedPiccadilly = "Between Oakwood and Cockfosters\nBetween Barons Court, Hounslow West and Uxbridge";
+const PartiallyAllowedElizabeth = "From west: until Royal Oak\nTo west: from Paddington\nFrom east: until Bethnal Green\nTo east: from Liverpool Street\nNote: until the Elizabeth Line fully opens (Spring 2023), the times are actually slightly <a target='_blank' href='https://lcc.org.uk/news/new-connections-for-cycles-on-trains/'>different</a>";
+const PartiallyAllowedOverground = "From Liverpool Street station going towards Chingford, Enfield Town or Cheshunt.\nComing from Chingford, Enfield Town or Cheshunt until Bethnal Green.";
+
 
 const lineColors = new Map([
-    [Bakerloo, "#B36305"],
-    [Central, "#E32017"],
-    [Circle, "#FFD300"],
-    [District, "#00782A"],
-    [DLR, "#00A4A7"],
-    [Elizabeth, "#9364CD"],
-    [HammersmithCity, "#F3A9BB"],
-    [Jubilee, "#A0A5A9"],
-    [Metropolitan, "#9B0056"],
-    [Northern, "#000000"],
-    [Overground, "#EE7C0E"],
-    [Piccadilly, "#003688"],
-    [Victoria, "#0098D4"],
-    [WaterlooCity, "#95CDBA"],
+    [Folded, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
+    [Bakerloo, { backgroundColor: "#B36305", textColor: "#ffffff" }],
+    [Central, { backgroundColor: "#E32017", textColor: "#ffffff" }],
+    [Circle, { backgroundColor: "#FFD300", textColor: "#ffffff" }],
+    [District, { backgroundColor: "#00782A", textColor: "#ffffff" }],
+    [DLR, { backgroundColor: "#00A4A7", textColor: "#ffffff" }],
+    [Elizabeth, { backgroundColor: "#9364CD", textColor: "#ffffff" }],
+    [HammersmithCity, { backgroundColor: "#F3A9BB", textColor: "#ffffff" }],
+    [Jubilee, { backgroundColor: "#A0A5A9", textColor: "#ffffff" }],
+    [Metropolitan, { backgroundColor: "#9B0056", textColor: "#ffffff" }],
+    [Northern, { backgroundColor: "#000000", textColor: "#ffffff" }],
+    [Overground, { backgroundColor: "#EE7C0E", textColor: "#ffffff" }],
+    [Piccadilly, { backgroundColor: "#003688", textColor: "#ffffff" }],
+    [Victoria, { backgroundColor: "#0098D4", textColor: "#ffffff" }],
+    [WaterlooCity, { backgroundColor: "#95CDBA", textColor: "#ffffff" }],
+    [BusTram, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
+    [NationalRail, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
+    [River, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
+    [EmiratedAir, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
+    [Coaches, { backgroundColor: "#FFFFFF", textColor: "#000000" }],
 ]);
 
 const pastMidnight_0000_0730 = "00:00-07:30";
@@ -40,37 +67,49 @@ const eveningPeak_1600_1900 = "16:00-19:00";
 const nighttime_1900_2400 = "19:00-00:00";
 
 const allowedSet = new Map([
-    [Bakerloo, "Between Queen's Park and Harrow & Wealdstone"],
-    [Central, "Between Leyton, Epping and Newbury Park (via Hainault only)\nBetween White City, Ealing Broadway and West Ruislip"],
-    [Circle, AllowedAnywhere],
-    [District, AllowedAnywhere],
-    [DLR, AllowedAnywhere],
-    [Elizabeth, AllowedAnywhere],
-    [HammersmithCity, AllowedAnywhere],
-    [Jubilee, "Between Stratford and Canning Town\nBetween Finchley Road and Stanmore"],
-    [Metropolitan, AllowedAnywhere],
-    [Northern, "Between Golders Green and Hendon Central\nBetween Colindale and Edgware\nBetween East Finchley, Mill Hill East and High Barnet"],
-    [Piccadilly, "Between Oakwood and Cockfosters\nBetween Barons Court, Hounslow West and Uxbridge"],
-    [Overground, AllowedAnywhere],
-    [Victoria, NotAllowed],
-    [WaterlooCity, NotAllowed],
+    [Folded, { message: AllowedFolded, showHours: false }],
+    [Bakerloo, { message: AllowedBakerloo, showHours: true }],
+    [Central, { message: AllowedCentral, showHours: true }],
+    [Circle, { message: AllowedAnywhere, showHours: true }],
+    [District, { message: AllowedAnywhere, showHours: true }],
+    [DLR, { message: AllowedAnywhere, showHours: true }],
+    [Elizabeth, { message: AllowedAnywhere, showHours: true }],
+    [HammersmithCity, { message: AllowedAnywhere, showHours: true }],
+    [Jubilee, { message: AllowedJubilee, showHours: true }],
+    [Metropolitan, { message: AllowedAnywhere, showHours: true }],
+    [Northern, { message: AllowedNorthern, showHours: true }],
+    [Piccadilly, { message: AllowedPiccadilly, showHours: true }],
+    [Overground, { message: AllowedAnywhere, showHours: true }],
+    [Victoria, { message: NotAllowedEver, showHours: false }],
+    [WaterlooCity, { message: NotAllowedEver, showHours: false }],
+    [BusTram, { message: NotAllowedBusesTrams, showHours: false }],
+    [NationalRail, { message: AllowedNationalRail, showHours: false }],
+    [River, { message: AllowedRiver, showHours: false }],
+    [EmiratedAir, { message: AllowedEmiratesAir, showHours: false }],
+    [Coaches, { message: AllowedCoaches, showHours: false }],
 ]);
 
 const notAllowedSet = new Map([
-    [Bakerloo, NotAllowed],
-    [Central, NotAllowed],
-    [Circle, NotAllowed],
-    [District, NotAllowed],
-    [DLR, NotAllowed],
-    [Elizabeth, "From west: until Royal Oak\nTo west: from Paddington\nFrom east: until Bethnal Green\nTo east: from Liverpool Street\nNote: until the Elizabeth Line fully opens (Spring 2023), the times are actually slightly <a target='_blank' href='https://lcc.org.uk/news/new-connections-for-cycles-on-trains/'>different</a>"],
-    [HammersmithCity, NotAllowed],
-    [Jubilee, NotAllowed],
-    [Metropolitan, NotAllowed],
-    [Northern, NotAllowed],
-    [Piccadilly, NotAllowed],
-    [Overground, "From Liverpool Street station going towards Chingford, Enfield Town or Cheshunt.\nComing from Chingford, Enfield Town or Cheshunt until Bethnal Green."],
-    [Victoria, NotAllowed],
-    [WaterlooCity, NotAllowed],
+    [Folded, { message: AllowedFolded, showHours: false }],
+    [Bakerloo, { message: NotAllowed, showHours: true }],
+    [Central, { message: NotAllowed, showHours: true }],
+    [Circle, { message: NotAllowed, showHours: true }],
+    [District, { message: NotAllowed, showHours: true }],
+    [DLR, { message: NotAllowed, showHours: true }],
+    [Elizabeth, { message: PartiallyAllowedElizabeth, showHours: true }],
+    [HammersmithCity, { message: NotAllowed, showHours: true }],
+    [Jubilee, { message: NotAllowed, showHours: true }],
+    [Metropolitan, { message: NotAllowed, showHours: true }],
+    [Northern, { message: NotAllowed, showHours: true }],
+    [Piccadilly, { message: NotAllowed, showHours: true }],
+    [Overground, { message: PartiallyAllowedOverground, showHours: true }],
+    [Victoria, { message: NotAllowedEver, showHours: false }],
+    [WaterlooCity, { message: NotAllowedEver, showHours: false }],
+    [BusTram, { message: NotAllowedBusesTrams, showHours: false }],
+    [NationalRail, { message: AllowedNationalRail, showHours: false }],
+    [River, { message: AllowedRiver, showHours: false }],
+    [EmiratedAir, { message: AllowedEmiratesAir, showHours: false }],
+    [Coaches, { message: AllowedCoaches, showHours: false }],
 ]);
 
 const weekdayConfig = new Map([
